fdceaa7df365f6d828bb32dffb21bc1f3f04f521
Mahin <touqeermahin915@gmail.com>
2025-06-29 19:02:10 +0530

    Update twitterController.js

--- server/controllers/twitterController.js
+++ server/controllers/twitterController.js
@@ -1,7 +1,10 @@
 const { createHashtagURL, createSearchURL } = require("../utils/URL_generator");
-const getTweets = require("../src/tweetScrapper");
+const { getTweets, getTweetsOfHandle } = require("../src/tweetScrapper");
 const ExpressError = require("../utils/ExpressError");
-
+const User = require("../models/User.js");
+const Article = require("../models/Article.js");
+const { getTwitterArticle } = require("../utils/utils.js");
+// const User = require("../models/user.js");
 module.exports.hashtag = async (req, res, next) => {
   try {
     const { query, limit, dateRange, selectedAiModel } = req.body;
@@ -30,10 +33,43 @@ module.exports.hashtag = async (req, res, next) => {
       throw new ExpressError(429, "API usage limit exceeded");
     }
 
-    // res.json({ summary: "This is a test summary" });
-
     const search_URL = createHashtagURL(query, dateRange);
-    await getTweets(search_URL, limit, res, req, query, selectedAiModel);
+
+    const tweets = await getTweets(search_URL, limit);
+
+    if (!tweets.length) {
+      res.status(500).json({
+        message: `No tweets found for this request`,
+      });
+      return;
+    }
+
+    const tempArticle = await getTwitterArticle(tweets, query, selectedAiModel);
+
+    res.json({ summary: tempArticle });
+
+    // Update API usage
+    console.log("updating user...");
+
+    await User.findByIdAndUpdate(req.user._id, { $inc: { apiUsage: 1 } });
+
+    console.log("updated user");
+
+    console.log("saving article...");
+
+    // Create and save article to database
+    const article = new Article({
+      userId: req.user._id,
+      content: tempArticle,
+      search: {
+        platform: "twitter",
+        query: query,
+      },
+    });
+
+    await article.save();
+
+    console.log("saved article");
   } catch (error) {
     next(error);
   }
@@ -62,7 +98,42 @@ module.exports.search = async function (req, res, next) {
     }
 
     const search_URL = createSearchURL(query, dateRange);
-    await getTweets(search_URL, limit, res, req, query);
+
+    const tweets = await getTweets(search_URL, limit);
+
+    if (!tweets.length) {
+      res.status(500).json({
+        message: `No tweets found for this request`,
+      });
+      return;
+    }
+
+    const tempArticle = await getTwitterArticle(tweets, query, selectedAiModel);
+
+    res.json({ summary: tempArticle });
+
+    // Update API usage
+    console.log("updating user...");
+
+    await User.findByIdAndUpdate(req.user._id, { $inc: { apiUsage: 1 } });
+
+    console.log("updated user");
+
+    console.log("saving article...");
+
+    // Create and save article to database
+    const article = new Article({
+      userId: req.user._id,
+      content: tempArticle,
+      search: {
+        platform: "twitter",
+        query: query,
+      },
+    });
+
+    await article.save();
+
+    console.log("saved article");
   } catch (error) {
     next(error);
   }
@@ -70,8 +141,7 @@ module.exports.search = async function (req, res, next) {
 
 module.exports.handle = async function (req, res, next) {
   try {
-    const { query, limit } = req.body;
-
+    const { query, limit, selectedAiModel } = req.body;
     if (!query) {
       throw new ExpressError(400, "Handle is required");
     }
@@ -84,18 +154,53 @@ module.exports.handle = async function (req, res, next) {
       throw new ExpressError(400, "Limit cannot exceed 50");
     }
 
-    if (query.includes(" ")) {
-      throw new ExpressError(400, "hashtag not in format");
-    }
+    // if (query.includes(" ")) {
+    //   throw new ExpressError(400, "hashtag not in format");
+    // }
 
     const { apiUsage, usageQuota } = req.user;
 
     if (apiUsage >= usageQuota) {
       throw new ExpressError(429, "API usage limit exceeded");
     }
+    const new_Query = query.split(" ").join("%20");
+    const search_URL = `https://api.twitterapi.io/twitter/user/last_tweets?userName=${new_Query}`;
 
-    const search_URL = `https://x.com/${query}?time=${Date.now()}`;
-    await getTweets(search_URL, limit, res, req, query);
+    const tweets = await getTweetsOfHandle(search_URL, limit);
+
+    if (!tweets.length) {
+      res.status(500).json({
+        message: `No tweets found for this request`,
+      });
+      return;
+    }
+
+    const tempArticle = await getTwitterArticle(tweets, query, selectedAiModel);
+
+    res.json({ summary: tempArticle });
+
+    // Update API usage
+    console.log("updating user...");
+
+    await User.findByIdAndUpdate(req.user._id, { $inc: { apiUsage: 1 } });
+
+    console.log("updated user");
+
+    console.log("saving article...");
+
+    // Create and save article to database
+    const article = new Article({
+      userId: req.user._id,
+      content: tempArticle,
+      search: {
+        platform: "twitter",
+        query: query,
+      },
+    });
+
+    await article.save();
+
+    console.log("saved article");
   } catch (error) {
     next(error);
   }
